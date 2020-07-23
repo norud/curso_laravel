390bb4ad158027459cf2325ccce15b0412329123
unknown <info@linkercr.com>
2020-07-23 17:23:23 -0600

    raw

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,45 @@ return 'Example url long'. route('admin.home');
 
 //routa que proporciona los metodos del crud
 //Route::resource('post', 'PostsController');
-Route::get('contact', 'PostsController@contact');
+//Route::get('contact', 'PostsController@contact');
 
-Route::get('post/{id}/{name}/{tk}', 'PostsController@show_posts');
+//Route::get('post/{id}/{name}/{tk}', 'PostsController@show_posts');
+//insertar raw en la bd
+/*
+Route::get('insert', function()
+{
+    DB::insert('insert into posts ( title, content) values (?, ?)', [
+        'Php laravel', 'Content about laravel']);
+});*/
+
+///leer datos en raw
+/*Route::get('read', function(){
+    $rs = DB::select('select * from posts where id = ?', [1]);
+
+    foreach($rs as $r){
+      echo $r->title.'<br>'.
+      $r->content;
+    }
+
+});*/
+
+//actualizar raw
+/*Route::get('update', function(){
+    $up = DB::update('update posts set title = "Update Title" where id = ?', [1]);
+    if($up){
+        return $up;
+    }else{
+        return 'Error';
+    }
+});*/
+
+//eliminar
+Route::get('eliminar', function(){
+    $del = DB::delete('delete from posts where id = ?', [1]);
+    if($del){
+        return $del;
+    }else{
+        return 'Error';
+    }
+});
+
