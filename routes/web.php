6795effb83ef78f6e9b8cfe0ab6c89d1b76ab027
unknown <info@linkercr.com>
2020-07-23 14:26:01 -0600

    Hasta Video 26

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//route with nickname
+Route::get('admin/long/urlpos/home', array('as' => 'admin.home', function(){
+    echo '<a href="'.route("admin.home").'">click </a>';
+return 'Example url long'. route('admin.home');
+}));
+//pasar un parametro directo url(get)
+//Route::get('post/{p}', 'PostsController@index');
+
+//routa que proporciona los metodos del crud
+Route::resource('post', 'PostsController');
