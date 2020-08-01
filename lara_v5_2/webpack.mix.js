27aef4a2f40fcc060f49821a433a76c52ac63354
unknown <info@linkercr.com>
2020-08-01 15:18:26 -0600

    to 5.4 not 5.5

--- lara_v5_2/webpack.mix.js
+++ lara_v5_2/webpack.mix.js
@@ -5,9 +5,6 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css');
-
-
-
 mix.styles([
     'resources/assets/css/libs/blog-post.css',
     'resources/assets/css/libs/bootstrap.css',
